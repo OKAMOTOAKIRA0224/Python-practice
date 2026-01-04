7d1c228b5c5f34b479ba9eb561a8bdb062c1d207
akira <0kinakomoti0@example.com>
2026-01-04 22:28:37 +0900

    制御構文(for文)5問目まで

--- 制御構文.py
+++ 制御構文.py
@@ -19,3 +19,31 @@ else:
         print("負の奇数です")
     else:
         print("整数です")
+
+#<制御構文(for文)>
+#1.下記リストの要素を先頭から順に出力せよ。
+names=["John","Kevin","Louis"]
+for i in names:
+    print(i)
+
+#2.range関数を用いて0~9を出力せよ。
+for i in range(10):
+    print(i)
+
+#3.追加でi=6の時「終了！」と出力してループを抜け出すよう実装せよ。
+for i in range(10):
+    if i==6:
+        print("終了！")
+        break 
+    print(i)
+
+#4.追加でi=3の時のみ出力結果が出ないように実装せよ。
+for i in range(10):
+  if i!=3:
+    print(i)
+
+#5.下記２つのリストを用意し、それぞれのリストから姓名を同時に出力せよ。
+lasts=["岡本","佐藤","田中"]
+firsts=["明","拓哉","太郎"]
+for a,b in zip(lasts,firsts):
+    print(a+b)
