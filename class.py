dfa1c9bdcca1ae9a787e546e7b9dab4576d91910
akira <0kinakomoti0@example.com>
2026-01-11 23:40:18 +0900

    class最後まで

--- class.py
+++ class.py
@@ -25,3 +25,28 @@ class Person:
 
 akira=Person("akira")
 print(akira.name)
+
+#3.先ほどのPersonクラスを継承したKidクラスを作成せよ。その際に自分の年齢を出力するように
+#クラスメソッドsay_hello(age)(年齢を引数)を変更せよ。
+
+class Kid(Person):
+    def say_hello(self,age):
+        print(f"私の名前は{self.name}です。年齢は{age}歳です。")
+Kid=Kid("akira")
+print(Kid.say_hello(18))
+
+#4.先ほどのPersonクラス内の変数nationalityを外部から呼び出せないprivateな変数に、
+#メソッドsay_my_name()も外部から呼び出せないprivateなメソッドに変更せよ。
+
+class Person:
+    __nationality="japan"
+
+    def __init__(self,name):
+        self.name=name
+
+    def say_hello(self):
+        print(f"こんにちは、私の国籍は{self.nationality}です。")
+    
+    def __say_my_name(self):
+        print(f"私の名前は{self.name}です")
+       
