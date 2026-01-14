0a615a608e6f6b04e231e9d45b60f69ba420fbc2
akira <0kinakomoti0@example.com>
2026-01-14 21:40:52 +0900

    os操作最後まで

--- os操作.py
+++ os操作.py
@@ -24,3 +24,22 @@ print(os.path.isdir("xyz"))
 
 #7.別の方法で５で確認したファイルの存在を確認せよ。
 print(os.path.isfile("例外処理.py"))
+
+#8.パス名の出力をos.path.join()で結合せよ
+for curDir,dird,files in os.walk("."):
+    for file in files:
+        print(os.path.join(curDir,file))
+
+#8.カレントディレクトリにakiraディレクトリを作成せよ。
+#o.mkdir("akira")
+#カレントディレクトリにあるsample.txtを削除せよ。
+#os.remove("sample.txt")
+
+#9.カレントディレクトリにあるakiraフォルダの名前を自分の名前に変更せよ。
+#os.rename("akira","岡本明")
+
+#10.環境変数であるPATHの確認せよ。
+os.environ["PATH"]
+
+#11.os.system()でUnixコマンドを使ってみましょう
+print(os.popen('ls -a').read())
