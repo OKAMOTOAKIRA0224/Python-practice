c55fb1eba31673053ae386c4cf84c528b8ac0390
akira <0kinakomoti0@example.com>
2026-01-18 22:26:58 +0900

    numpy最後まで

--- ベクトル.py
+++ ベクトル.py
@@ -17,5 +17,34 @@ print(d)
 e=np.eye(5)
 print(e)
 
-#
-#
+#4.次の計算をせよ
+#A=1 5 2-1
+#  4-3,7 6
+#のときA+B,A-B,AB
+A=np.array([[1,5],[4,-2]])
+B=np.array([[2,-1],[7,6]])
+print(A+B)
+print(A-B)
+print(np.dot(A,B))
+
+#5.以下の行列から合計値、最大値、最小値、平均値、中央値、分散、標準偏差を取得せよ。
+#  1 5 -2 
+#A=4 0 -3
+# -8 2  6
+C=np.array([[1,5,-2],[4,0,-3],[-8,2,6]])
+print(C.sum())
+print(C.max())
+print(C.min())
+print(np.average(C))
+print(np.median(C))
+print(np.var(C))
+print(np.std(C))
+
+#6.以下の行列に逆行列がある場合その行列を求めよ
+#A=1 5
+#  4-2
+D=([[1,5],[4,-2]])
+d=np.linalg.det(D)
+print(d)
+d_inv=np.linalg.inv(D)
+print(np.dot(D,d_inv))
